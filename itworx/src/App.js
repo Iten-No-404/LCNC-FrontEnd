d71a46c0cd676a184d3e1306f8ab27597c297481
Raghad-Khaled <Raghoda123492000@gmail.com>
2022-07-26 00:01:12 +0200

    Starter Code

---
+++ itworx/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <div className="App">
+        ItWorx Summer 2022
+      </div>
+    </>
+  );
+}
+
+export default App;
