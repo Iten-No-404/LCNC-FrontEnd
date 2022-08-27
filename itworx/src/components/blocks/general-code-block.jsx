8233cf8cd522c114e54ef3967130b4fabd5a1787
raghad <raghad.khadra00@eng-st.cu.edu.eg>
2022-08-27 15:16:06 +0200

    all blocks get from backend

--- itworx/src/components/blocks/general-code-block.jsx
+++ itworx/src/components/blocks/general-code-block.jsx
@@ -3,16 +3,14 @@ import { useSelector } from "react-redux";
 import { selectDefaultCSS } from "../../states/default-css-slice/default-css-slice";
 import parse from 'html-react-parser';
 
-export default function GeneralCodeBlock({ text, id, classN, font = '', CSS, logo = false, isDragging = false, code1, code2 }) {
+export default function GeneralCodeBlock({ classN,text, CSS=null, isDragging = false, code1, code2 }) {
     const defaultCSS = useSelector(selectDefaultCSS);
 
-    if (logo) {
+    if (!CSS) {
         CSS = defaultCSS;
-        font = defaultCSS.font;
     }
 
     const styling = {
-        "border": isDragging ? "5px solid pink" : "0px",
         "font-family": CSS.font.family,
         "color": CSS.color,
         "font-size": CSS.font.size + "px",
@@ -39,5 +37,5 @@ export default function GeneralCodeBlock({ text, id, classN, font = '', CSS, log
         code2
     );
     console.log(codeText)
-    return parse(codeText);    
+    return <div className={classN} style={{"border": isDragging ? "5px solid pink" : "0px"}}>{parse(codeText)}</div>
 }
--- itworx/src/components/tabs/tabs.jsx
+++ itworx/src/components/tabs/tabs.jsx
@@ -1,14 +1,12 @@
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
-import Header from '../blocks/header';
-import Section from '../blocks/section';
-import Nav from '../blocks/nav';
 import Layers from '../layers/layers';
-import DisplayImage from '../blocks/image'
 import StyledBlock from '../style-block/style-block';
 import { Droppable, Draggable } from "react-beautiful-dnd";
-import Div from '../blocks/div';
 import { PropTypes } from "prop-types";
+import { useSelector } from "react-redux";
+import { selectBlocksList } from '../../states/blocks-list-slice/blocks-list-slice';
+import GeneralCodeBlock from '../blocks/general-code-block';
 
 /**
  * That the side menue that show three Tabs 
@@ -20,6 +18,8 @@ import { PropTypes } from "prop-types";
  *3- third one show the styled block comonent
  */
 function Ctabs({ board, setBoard }) {
+  const blocksList = useSelector(selectBlocksList);
+  console.log(blocksList);
   return (
     <div>
       <Droppable droppableId="selectWidgetTab" isDropDisabled={true} type="board" >
@@ -32,66 +32,24 @@ function Ctabs({ board, setBoard }) {
               justify
             >
               <Tab eventKey="widgets" title="Blocks" className='Blocks'>
-              <Draggable draggableId='5' key={5} index={5} >
-                  {(provided, snapshot) => (
-                    <>
-                      <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} className="mt-3">
-                        <Div isDragging={snapshot.isDragging} classN="blockTab" id={5} text="Div" logo={true}/>
-                      </div>
-                      {snapshot.isDragging && (
-                        <Div classN="blockTab" id={5} text="Div" logo={true} />
+                {
+                  blocksList.map((block)=>{
+                    return (
+                      <Draggable draggableId={block.id} key={block.id} index={block.id} >
+                      {(provided, snapshot) => (
+                        <>
+                          <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} className="mt-3">
+                            <GeneralCodeBlock isDragging={snapshot.isDragging} classN="blockTab" id={block.id} text={block.text} code1={block.code1} code2={block.code2}/>
+                          </div>
+                          {snapshot.isDragging && (
+                            <GeneralCodeBlock classN="blockTab" id={block.id} text={block.text} code1={block.code1} code2={block.code2} />
+                          )}
+                        </>
                       )}
-                    </>
-                  )}
-                </Draggable>
-                <Draggable draggableId='1' key={1} index={1}>
-                  {(provided, snapshot) => (
-                    <>
-                      <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}  className="mt-3">
-                        <Header isDragging={snapshot.isDragging} classN="blockTab" text="Header" logo={true} />
-                      </div>
-                      {snapshot.isDragging && (
-                        <Header classN="blockTab" text="Header" id='headerlogo' logo={true} />
-                      )}
-                    </>
-                  )}
-                </Draggable>
-                <Draggable draggableId='2' key={2} index={2}>
-                  {(provided, snapshot) => (
-                    <>
-                      <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}  className="mt-3">
-                        <Section isDragging={snapshot.isDragging} classN="blockTab" text="Section" id={2} logo={true} />
-                      </div>
-                      {snapshot.isDragging && (
-                        <Section classN="blockTab" text="Section" id={2} logo={true} />
-                      )}
-                    </>
-                  )}
-                </Draggable>
-                <Draggable draggableId='3' key={3} index={3}>
-                  {(provided, snapshot) => (
-                    <>
-                      <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}  className="mt-3">
-                        <DisplayImage isDragging={snapshot.isDragging} classN="blockTab" id={3} logo={true}/>
-                      </div>
-                      {snapshot.isDragging && (
-                        <DisplayImage classN="blockTab" id={3}  logo={true}/>
-                      )}
-                    </>
-                  )}
-                </Draggable>
-                <Draggable draggableId='4' key={4} index={4}>
-                  {(provided, snapshot) => (
-                    <>
-                      <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}  className="mt-3">
-                        <Nav isDragging={snapshot.isDragging} classN="blockTab" id={4} text="Bootstrap" logo={true}/>
-                      </div>
-                      {snapshot.isDragging && (
-                        <Nav classN="blockTab" id={4} text="Bootstrap" logo={true} />
-                      )}
-                    </>
-                  )}
-                </Draggable>
+                    </Draggable>
+                    )
+                  })
+                }
               </Tab>
               <Tab eventKey="layers" title="Layers">
                 <Layers board={board} setBoard={setBoard} />
